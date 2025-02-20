7fd69d6d6897177159c0d7ba871202cf41bab789
Seoyeon Lee <29548086+sel041@users.noreply.github.com>
2025-02-20 14:09:17 -0800

    Update 05_Age_AD_DEG.R

--- 02_gene_expression_analysis/05_Age_AD_DEG.R
+++ 02_gene_expression_analysis/05_Age_AD_DEG.R
@@ -24,7 +24,7 @@ indir <- '~/5XFAD_mouse/data/'
 outdir <- '~/mouseAD/'
 ct <- 'Microglia'
 
-# AD temporal changes 
+# temporal changes
 # genotype contrasts
 genotype_contrast <- 'DGE_contrasts_genotype_age'
 genotype_files <- c('ADvsWT.18M.tsv', 'ADvsWT.9M.tsv', 'ADvsWT.3M.tsv')
@@ -37,6 +37,46 @@ ADvsWT.18M <- ADvsWT.18M[ADvsWT.18M$FDR < 0.05, ]
 ADvsWT.9M <- ADvsWT.9M[ADvsWT.9M$FDR < 0.05, ]
 ADvsWT.3M <- ADvsWT.3M[ADvsWT.3M$FDR < 0.05, ]
 
-ADvsWT.18M <- ADvsWT.18M[!grepl("^Gm|^Rpl|^Rps|^Mrps|^Mrpl", rownames(ADvsWT.18M)), ] 
-ADvsWT.9M <- ADvsWT.9M[!grepl("^Gm|^Rpl|^Rps|^Mrps|^Mrpl", rownames(ADvsWT.9M)), ]
-ADvsWT.3M <- ADvsWT.3M[!grepl("^Gm|^Rpl|^Rps|^Mrps|^Mrpl", rownames(ADvsWT.3M)), ]
+# up genes
+ADvsWT.18M.up <- rownames(ADvsWT.18M[ADvsWT.18M$logFC > 0, ])
+ADvsWT.9M.up <- rownames(ADvsWT.9M[ADvsWT.9M$logFC > 0, ])
+ADvsWT.3M.up <- rownames(ADvsWT.3M[ADvsWT.3M$logFC > 0, ])
+
+ADvsWT.early.up <- Reduce(intersect, list(ADvsWT.3M.up, ADvsWT.9M.up, ADvsWT.18M.up))
+ADvsWT.mid.up <- setdiff(intersect(ADvsWT.9M.up,ADvsWT.18M.up), ADvsWT.early.up)
+ADvsWT.late.up <- setdiff(ADvsWT.18M.up, c(ADvsWT.early.up, ADvsWT.mid.up))
+
+# down genes
+ADvsWT.18M.down <- rownames(ADvsWT.18M[ADvsWT.18M$logFC < 0, ])
+ADvsWT.9M.down <- rownames(ADvsWT.9M[ADvsWT.9M$logFC < 0, ])
+ADvsWT.3M.down <- rownames(ADvsWT.3M[ADvsWT.3M$logFC < 0, ])
+
+ADvsWT.early.down <- Reduce(intersect, list(ADvsWT.3M.down, ADvsWT.9M.down, ADvsWT.18M.down))
+ADvsWT.mid.down <- setdiff(intersect(ADvsWT.9M.down,ADvsWT.18M.down), ADvsWT.early.down)
+ADvsWT.late.down <- setdiff(ADvsWT.18M.down, c(ADvsWT.early.down, ADvsWT.mid.down))
+
+# AD- and age-associated DEGs
+genotype_contrast <- 'DGE_contrasts_genotype_age'
+genotype_files <- c('ADvsWT.18M.tsv', 'ADvsWT.9M.tsv', 'ADvsWT.3M.tsv')
+ADvsWT.18M <- read.table(paste0(indir,genotype_contrast,"/",ct,"/","ADvsWT.18M.tsv"))
+ADvsWT.18M <- ADvsWT.18M[ADvsWT.18M$FDR < 0.05, ]
+
+age_files <- c(#'AD.18Mvs3M.tsv', 'AD.18Mvs9M.tsv', 'AD.9Mvs3M.tsv',
+               'WT.18Mvs3M.tsv', 'WT.18Mvs9M.tsv', 'WT.9Mvs3M.tsv')
+WT.18Mvs3M <- read.table(paste0(indir,genotype_contrast,"/",ct,"/","WT.18Mvs3M.tsv"))
+WT.18Mvs3M <- WT.18Mvs3M[WT.18Mvs3M$FDR < 0.05, ]
+
+# up genes
+ADvsWT.18M.up <- rownames(ADvsWT.18M[ADvsWT.18M$logFC > 0, ])
+WT.18Mvs3M.up <- rownames(WT.18Mvs3M[WT.18Mvs3M$logFC > 0, ])
+AD_Age_up <- Reduce(intersect, list(ADvsWT.18M.up, WT.18Mvs3M.up))
+AD_up <- setdiff(ADvsWT.18M.up,AD_Age_up)
+Age_up <- setdiff(WT.18Mvs3M.up, AD_Age_up)
+
+# down genes
+ADvsWT.18M.down <- rownames(ADvsWT.18M[ADvsWT.18M$logFC < 0, ])
+WT.18Mvs3M.down <- rownames(WT.18Mvs3M[WT.18Mvs3M$logFC < 0, ])
+
+AD_Age_down <- Reduce(intersect, list(ADvsWT.18M.down, WT.18Mvs3M.down))
+AD_down <- setdiff(ADvsWT.18M.down,AD_Age_down)
+Age_down <- setdiff(WT.18Mvs3M.down, AD_Age_down)

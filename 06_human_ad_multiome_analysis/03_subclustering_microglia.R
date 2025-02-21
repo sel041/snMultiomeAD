4f8c6ca859af2dd3daf7fedf6ec496d64fd150c5
Seoyeon Lee <29548086+sel041@users.noreply.github.com>
2025-02-21 12:16:49 -0800

    Create 03_subclustering_microglia.R

---
+++ 06_human_ad_multiome_analysis/03_subclustering_microglia.R
@@ -0,0 +1,49 @@
+set.seed(1234)
+library(Signac)
+library(Seurat)
+library(JASPAR2024)
+library(TFBSTools)
+library(BSgenome.Hsapiens.UCSC.hg38)
+library(patchwork)
+
+setwd("~/humanAD")
+h_sig <- readRDS("~/humanAD_ATAC_signac_object.rds")
+hmgc_sig <- subset(h151, idents=c("Microgila"))
+
+# get .jaspar file paths
+jaspar_dir <- "~/JASPAR2024"
+jaspar_files <- list.files(jaspar_dir, pattern = "\\.jaspar$", full.names = TRUE)
+
+# read all files and store PFMatrix objects in a list
+pfm_list <- lapply(jaspar_files, function(file) {
+  tryCatch({
+    readJASPARMatrix(file)
+  }, error = function(e) {
+    message("Error reading file: ", file, " - ", e$message)
+    NULL
+  })
+})
+
+# remove NULL entries 
+pfm_list <- Filter(Negate(is.null), pfm_list)
+
+# combine into a PFMatrixList
+pfm_matrix_list <- do.call(c, pfm_list)
+pfm_motif_names <- names(pfm_matrix_list)
+
+# set names based on IDs
+names(pfm_matrix_list) <- sapply(pfm_list, function(x) ID(x))
+pfm <- names(pfm_matrix_list)
+
+# add motif information
+hmgc_sig <- AddMotifs(
+  object = hmgc_sig,
+  genome = BSgenome.Hsapiens.UCSC.hg38,
+  pfm = pfm
+)
+
+# computing motif activities
+hmgc_sig <- RunChromVAR(
+  object = hmgc_sig,
+  genome = BSgenome.Hsapiens.UCSC.hg38
+)
